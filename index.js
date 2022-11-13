8189b36d14b375b27f76515c5774157059e938a1
IniguezMarc <miniguezbarbero@gmail.com>
2022-11-13 16:48:14 +0100

    scratch

--- index.js
+++ index.js
@@ -23,39 +23,6 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-function randomIntFromInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-rain();
-function rain() {
-  const numRain = 3;
-  const rainItems = [
-    "angular.png",
-    "code.webp",
-    "html.png",
-    "html2.png",
-    "js.png",
-    "node.webp",
-    "react.png",
-    "sql.png",
-  ];
-  const body = document.querySelector("body");
-  rainItems.forEach((element) => {
-    for (let index = 0; index < numRain; index++) {
-      let rainElement = document.createElement("img");
-      rainElement.classList.add("rainElement");
-      rainElement.src = "assets/rain/" + element;
-      rainElement.alt = "";
-      rainElement.style.top = Math.random() * 100 + "%";
-      rainElement.style.animation =
-        "rainAnimation " + randomIntFromInterval(20, 40) + "s 2";
-      rainElement.style.animationIterationCount = "infinite";
-      rainElement.style.animationDelay = "-100s";
-      body.appendChild(rainElement);
-    }
-  });
-}
 
   document.querySelector('.themeSwitch').addEventListener("click", function(e) {
   let theme = document.documentElement.getAttribute('data-theme');
