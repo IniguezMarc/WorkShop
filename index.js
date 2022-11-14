75c682c3e97391cb6b283fd0fac1c3a45b743480
IniguezMarc <miniguezbarbero@gmail.com>
2022-11-14 14:21:20 +0100

    svg and responsive

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      console.log(entry.target)
-      if ( entry.target.hasAttribute('typed')) {
+      console.log(entry.target);
+      if (entry.target.hasAttribute("typed")) {
         entry.target.classList.add("typing-animation");
         let length = entry.target.textContent.length;
-        entry.target.style.animationTimingFunction = 'steps('+length+',end)';
-        entry.target.style.animationDuration = '1s';
-      }else{
+        entry.target.style.animationTimingFunction =
+          "steps(" + length + ",end)";
+        entry.target.style.animationDuration = "1s";
+      } else {
         entry.target.classList.add("show");
       }
     } else {
-      if ( entry.target.hasAttribute('typed')) {
+      if (entry.target.hasAttribute("typed")) {
         entry.target.classList.remove("typing-animation");
-      }else{
+      } else {
         entry.target.classList.remove("show");
       }
     }
@@ -23,17 +24,13 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
 
-
-  document.querySelector('.themeSwitch').addEventListener("click", function(e) {
-  let theme = document.documentElement.getAttribute('data-theme');
+document.querySelector(".themeSwitch").addEventListener("click", function (e) {
+  let theme = document.documentElement.getAttribute("data-theme");
   if (theme == "dark") {
-      document.documentElement.setAttribute('data-theme', 'light');
-      e.target.src = "assets/icons/bulbOff.png";
+    document.documentElement.setAttribute("data-theme", "light");
+    e.target.src = "assets/icons/bulbOff.png";
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    e.target.src = "assets/icons/bulbOn.png";
   }
-  else {
-      document.documentElement.setAttribute('data-theme', 'dark');
-      e.target.src = "assets/icons/bulbOn.png";
-  }    
 });
-
-
